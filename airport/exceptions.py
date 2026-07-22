e453c03c70b5fb7cb3a2332a31e5b976b84591f0
SamuelRojas09 <samrojas@unal.edu.co>
2026-07-22 00:20:46 -0500

    Update exceptions.py
    Se agrega OverweightLuggageError, excepcion que se lanza cuando el equipaje excede el peso maximo permitido. Se agrega EmptyQueueError, excepcion que se lanza cuando se intenta obtener un pasajero de una fila vacia

--- airport/exceptions.py
+++ airport/exceptions.py
@@ -1,3 +1,11 @@
 class FlightFullError(Exception):
     def __init__(self, flight_code: str) -> None:
         super().__init__(f"El vuelo {flight_code} no tiene cupos disponibles.")
+
+class OverweightLuggageError(Exception):
+    def __init__(self, weight: float, max_weight: float, luggage_type: str) -> None:
+        super().__init__(f"{luggage_type} pesa {weight} kg, excede el maximo permitido de {max_weight} kg.")
+
+class EmptyQueueError(Exception):
+    def __init__(self) -> None:
+        super().__init__("No hay pasajeros en la fila de espera.")
