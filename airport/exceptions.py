0410ec3cd8ec6eb5d5792aa1cb56b92677ebba6f
SamuelRojas09 <samrojas@unal.edu.co>
2026-07-22 11:40:40 -0500

    Update exceptions.py
    Se modifica formato para cumplir Ruff

--- airport/exceptions.py
+++ airport/exceptions.py
@@ -1,11 +1,20 @@
 class FlightFullError(Exception):
     def __init__(self, flight_code: str) -> None:
-        super().__init__(f"flight {flight_code} has no seats available.")
+        super().__init__(f"Flight {flight_code} has no seats available.")
 
 
 class OverweightLuggageError(Exception):
-    def __init__(self, weight: float, max_weight: float, luggage_type: str) -> None:
-        super().__init__(f"{luggage_type} weighs {weight} kg, exceeding the maximum allowed weight of {max_weight} kg.")
+    def __init__(
+        self,
+        weight: float,
+        max_weight: float,
+        luggage_type: str,
+    ) -> None:
+        super().__init__(
+            f"{luggage_type} weighs {weight} kg, "
+            f"exceeding the maximum allowed weight of {max_weight} kg."
+        )
+
 
 class EmptyQueueError(Exception):
     def __init__(self) -> None:
